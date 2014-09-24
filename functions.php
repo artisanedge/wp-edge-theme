a37c8108a373e49f297fb02d3909a429edeb3240
Kenichi Inoue <inoue@artisanedge.jp>
2014-09-24 13:58:26 +0900

    Delete _s ui javascript

--- functions.php
+++ functions.php
@@ -5,13 +5,6 @@
  * @package edge
  */
 
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
-}
-
 if ( ! function_exists( 'edge_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -93,10 +86,6 @@ add_action( 'widgets_init', 'edge_widgets_init' );
  */
 function edge_scripts() {
 	wp_enqueue_style( 'edge-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'edge-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'edge-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
